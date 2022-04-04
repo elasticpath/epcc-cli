0ca394376c1918d5ec1f50414d4ca2c1f7a55367
Matthew Kelly <matthew.kelly@elasticpath.com>
2022-04-04 12:26:18 -0400

    resolves #5 - add query params to get

--- cmd/get.go
+++ cmd/get.go
@@ -2,11 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/elasticpath/epcc-cli/external/json"
 	"github.com/elasticpath/epcc-cli/external/resources"
 	"github.com/spf13/cobra"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 	"time"
@@ -29,27 +31,37 @@ var get = &cobra.Command{
 		}
 
 		// Count ids in get-collection
-		url := resource.GetCollectionInfo.Url
-		idCount := strings.Count(url, "%")
+		resourceURL := resource.GetCollectionInfo.Url
+		idCount := strings.Count(resourceURL, "%")
 
 		// Determine if call should be get-collection or get-entity
 		if (idCount-len(args)+1)%2 != 0 {
 			idCount += 1
-			url = resource.GetEntityInfo.Url
+			resourceURL = resource.GetEntityInfo.Url
 		}
 
-		// Replace ids with args in url
+		// Replace ids with args in resourceURL
 		for i := 1; i <= idCount; i++ {
-			url = strings.Replace(url, "%"+strconv.Itoa(i), args[i], 1)
+			resourceURL = strings.Replace(resourceURL, "%"+strconv.Itoa(i), args[i], 1)
+		}
+
+		// Add remaining args as query params
+		params := url.Values{}
+		for i := idCount + 1; i+1 < len(args); i = i + 2 {
+			params.Add(args[i], args[i+1])
+		}
+		resourceURL = resourceURL + "?" + params.Encode()
+		if (idCount-len(args)+1)%2 != 0 {
+			resourceURL = resourceURL + url.QueryEscape(args[len(args)-1])
 		}
 
 		// Create the GET request
-		req, err := http.NewRequest("GET", Envs.EPCC_API_BASE_URL+url, nil)
+		req, err := http.NewRequest("GET", Envs.EPCC_API_BASE_URL+resourceURL, nil)
 		if err != nil {
 			return fmt.Errorf("Got error %s", err.Error())
 		}
 		req.Header.Set("user-agent", "golang application")
-		req.Header.Set("Authorization", "Bearer: 951a9a5db7b712ce999c3ccdac889805071c1ae7")
+		req.Header.Set("Authorization", "Bearer: 86afed525eab0255c8690223ce02b787707ed38a")
 
 		// Submit request
 		resp, err := client.Do(req)
@@ -69,7 +81,7 @@ var get = &cobra.Command{
 			log.Fatal(err)
 		}
 
-		fmt.Println(string(body))
+		json.PrintJson(string(body))
 
 		return nil
 	},
