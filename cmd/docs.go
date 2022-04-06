5725e4c06fcb468254bde47a4cedc565f6851e84
Parisa <parisa.khazaei@elasticpath.com>
2022-04-06 14:57:24 -0400

    advance resource doc

--- cmd/docs.go
+++ cmd/docs.go
@@ -7,21 +7,51 @@ import (
 )
 
 var docsCommand = &cobra.Command{
-	Use:   "docs <resource>",
+	Use:   "docs [RESOURCE] [ID_1]",
 	Short: "Opens up API documentation for the resource",
+	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 0 {
+		arg := len(args)
+
+		switch arg {
+		case 0:
+			return fmt.Errorf("You must supply a resource type to the docs command")
+		case 1:
 			resource := resources.Resources[args[0]]
 			if len(resource.Docs) > 0 {
-				url := resource.Docs
-				err := OpenUrl(url)
-				if err != nil {
-					return nil
-				}
+				openDoc(args[0], "")
 			} else {
 				return fmt.Errorf("You must supply a valid resource type to the docs command")
 			}
+		case 2:
+			resource := args[0]
+			verb := args[1]
+			openDoc(resource, verb)
+		default:
+			return fmt.Errorf("unsupported platform")
 		}
-		return fmt.Errorf("You must supply a resource type to the docs command")
+		return nil
 	},
+}
+
+func openDoc(resource string, verb string) error {
+	resourceDoc := resources.Resources[resource]
+
+	switch verb {
+	case "":
+		OpenUrl(resourceDoc.Docs)
+	case "get-collection":
+		OpenUrl(resourceDoc.GetCollectionInfo.Docs)
+	case "get-entity":
+		OpenUrl(resourceDoc.GetEntityInfo.Docs)
+	case "update-entity":
+		OpenUrl(resourceDoc.UpdateEntityInfo.Docs)
+	case "delete-entity":
+		OpenUrl(resourceDoc.DeleteEntityInfo.Docs)
+	case "create-entity":
+		OpenUrl(resourceDoc.CreateEntityInfo.Docs)
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
+	return nil
 }
