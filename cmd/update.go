c75d6fa618bb4ff439f86795155ef3ad9c07fdea
Matthew Kelly <40244217+epmkelly@users.noreply.github.com>
2022-04-07 11:54:49 -0400

    resolves #60 - attribute keys for create/update (#78)
    * resolves #60 - attribute keys for create/update
    
    * Fix panic when method is missing
    
    * Update completions to check for links aka create only recommends resources that can be created

--- cmd/update.go
+++ cmd/update.go
@@ -74,7 +74,30 @@ var update = &cobra.Command{
 		if len(args) == 0 {
 			return completion.Complete(completion.Request{
 				Type: completion.CompleteSingularResource,
+				Verb: completion.Update,
 			})
+		}
+
+		// Find Resource
+		resource, ok := resources.GetResourceByName(args[0])
+		if ok {
+			if resource.UpdateEntityInfo != nil {
+				idCount, _ := resources.GetNumberOfVariablesNeeded(resource.UpdateEntityInfo.Url)
+				if len(args)-idCount >= 1 { // Arg is after IDs
+					if (len(args)-idCount)%2 == 1 { // This is an attribute key
+						usedAttributes := make(map[string]int)
+						for i := idCount + 1; i < len(args); i = i + 2 {
+							usedAttributes[args[i]] = 0
+						}
+						return completion.Complete(completion.Request{
+							Type:       completion.CompleteAttributeKey,
+							Resource:   resource,
+							Attributes: usedAttributes,
+							Verb:       completion.Update,
+						})
+					}
+				}
+			}
 		}
 
 		return []string{}, cobra.ShellCompDirectiveNoFileComp
--- external/completion/completion.go
+++ external/completion/completion.go
@@ -14,9 +14,18 @@ const (
 	CompleteCrudAction       = 32
 )
 
+const (
+	Get    = 1
+	Create = 2
+	Update = 4
+	Delete = 8
+)
+
 type Request struct {
-	Type     int
-	Resource string
+	Type       int
+	Resource   resources.Resource
+	Attributes map[string]int
+	Verb       int
 }
 
 func Complete(c Request) ([]string, cobra.ShellCompDirective) {
@@ -24,13 +33,39 @@ func Complete(c Request) ([]string, cobra.ShellCompDirective) {
 
 	if c.Type&CompletePluralResource > 0 {
 		for k := range resources.GetPluralResources() {
-			results = append(results, k)
+			r, _ := resources.GetResourceByName(k) // Not worried about the bool here as resources come from the list already
+			if c.Verb&Get > 0 {
+				if r.GetCollectionInfo != nil {
+					results = append(results, k)
+				}
+			} else {
+				results = append(results, k)
+			}
 		}
 	}
 
 	if c.Type&CompleteSingularResource > 0 {
 		for _, v := range resources.GetSingularResourceNames() {
-			results = append(results, v)
+			r, _ := resources.GetResourceByName(v) // Not worried about the bool here as resources come from the list already
+			if c.Verb&Create > 0 {
+				if r.CreateEntityInfo != nil {
+					results = append(results, v)
+				}
+			} else if c.Verb&Update > 0 {
+				if r.UpdateEntityInfo != nil {
+					results = append(results, v)
+				}
+			} else if c.Verb&Delete > 0 {
+				if r.DeleteEntityInfo != nil {
+					results = append(results, v)
+				}
+			} else if c.Verb&Get > 0 {
+				if r.GetEntityInfo != nil {
+					results = append(results, v)
+				}
+			} else {
+				results = append(results, v)
+			}
 		}
 	}
 
@@ -39,7 +74,11 @@ func Complete(c Request) ([]string, cobra.ShellCompDirective) {
 	}
 
 	if c.Type&CompleteAttributeKey > 0 {
-		// do something with :resources.Resources[c.Resource].Attributes
+		for k := range c.Resource.Attributes {
+			if _, ok := c.Attributes[k]; !ok {
+				results = append(results, k)
+			}
+		}
 	}
 
 	return results, cobra.ShellCompDirectiveNoFileComp
