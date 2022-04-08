aa81feb504c11850d919f5518c05e04a786740af
Mahsa ghoreishi <mahsa.ghoreishi@elasticpath.com>
2022-04-08 09:17:41 -0400

    issue 57 json schemas

--- external/resources/resources_test.go
+++ external/resources/resources_test.go
@@ -2,7 +2,11 @@ package resources
 
 import (
 	"fmt"
+	"github.com/santhosh-tekuri/jsonschema/v4"
+	log "github.com/sirupsen/logrus"
 	"github.com/yosida95/uritemplate/v3"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"strings"
 	"testing"
 )
@@ -96,5 +100,26 @@ func TestUriTemplatesAllReferenceValidResource(t *testing.T) {
 
 	if len(errors) > 0 {
 		t.Fatalf("Errors occurred while validating URI Templates:\n%s", errors)
+	}
+}
+
+func TestJsonSchemaValidate(t *testing.T) {
+	sch, err := jsonschema.Compile("resources_schema.json")
+	if err != nil {
+		log.Fatalf("%#v", err)
+	}
+
+	data, err := ioutil.ReadFile("resources.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var v interface{}
+	if err := yaml.Unmarshal(data, &v); err != nil {
+		log.Fatal(err)
+	}
+
+	if err = sch.ValidateInterface(v); err != nil {
+		log.Fatalf("%#v", err)
 	}
 }
