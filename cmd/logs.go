192271decb1a03829ca698c0cc595986c6445284
Mahsa ghoreishi <mahsa.ghoreishi@elasticpath.com>
2022-04-11 14:07:27 -0400

    fix

--- cmd/logs.go
+++ cmd/logs.go
@@ -8,35 +8,45 @@ import (
 	"strings"
 )
 
-var logs = &cobra.Command{
-	Use:   "logs <VERB> [NUMBER]",
-	Short: "Returns Http logs",
+var logsClear = &cobra.Command{
+	Use:   "clear",
+	Short: "Clears all Http logs",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		os.RemoveAll(shared.LogDirectory)
+		return nil
+	},
+}
+
+var logsList = &cobra.Command{
+	Use:   "list",
+	Short: "List All Http logs",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		files := shared.AllFilesSortedByDate(shared.LogDirectory)
+		for i := 0; i < len(files); i++ {
+			name, _ := shared.Base64DecodeStripped(files[i].Name())
+			fmt.Println(name)
+		}
+		return nil
+	},
+}
+
+var logsShow = &cobra.Command{
+	Use:   "show <NUMBER>",
+	Short: "Show Http logs for specific number",
 	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		files := shared.AllFilesSortedByDate(shared.LogDirectory)
-		if args[0] == "show" {
-			if len(args) != 2 {
-				return fmt.Errorf("Show command needs the number of log to show")
+		for i := 0; i < len(files); i++ {
+			name, _ := shared.Base64DecodeStripped(files[i].Name())
+			segments := strings.Split(name, " ")
+			if segments[0] == args[0] {
+				fmt.Println(name)
+				break
 			}
-		}
-		if args[0] == "show" || args[0] == "list" {
-			for i := 0; i < len(files); i++ {
-				name, _ := shared.Base64DecodeStripped(files[i].Name())
-
-				if args[0] == "show" {
-					segments := strings.Split(name, " ")
-					if segments[0] == args[1] {
-						fmt.Println(name)
-						break
-					}
-				} else if args[0] == "list" {
-					fmt.Println(name)
-				}
-			}
-		} else if args[0] == "clear" {
-			os.RemoveAll(shared.LogDirectory)
 		}
 
 		return nil
 	},
 }
+
+var logs = &cobra.Command{Use: "logs"}
--- cmd/root.go
+++ cmd/root.go
@@ -33,6 +33,7 @@ func init() {
 		logs,
 		resourceListCommand,
 	)
+	logs.AddCommand(logsList, logsShow, logsClear)
 
 	testJson.Flags().BoolVarP(&noWrapping, "no-wrapping", "", false, "if set, we won't wrap the output the json in a data tag")
 	testJson.Flags().BoolVarP(&compliant, "compliant", "", false, "if set, we wrap most keys in an attributes tage automatically.")
