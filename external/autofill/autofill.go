6eebdea782d4fa7686a891c06dbdd89ecb83c36a
Steve Ramage <49958178+steve-r-west@users.noreply.github.com>
2023-04-28 17:23:23 -0700

    Resolves #298 - Introduce support for custom items (#299)

--- external/autofill/autofill.go
+++ external/autofill/autofill.go
@@ -25,12 +25,48 @@ func GetJsonArrayForResource(r *resources.Resource) []string {
 
 			v := reflect.ValueOf(faker)
 			methodName := strings.Trim(autofill[5:], " ")
+
+			var rejectZero = false
+			if strings.HasPrefix(methodName, "NonZero") {
+				methodName = methodName[7:]
+				rejectZero = true
+			}
+
 			method := v.MethodByName(methodName)
 			if method.IsValid() {
 				result := method.Call([]reflect.Value{})
 				if len(result) == 1 {
 
-					arg := result[0].String()
+					var arg string
+
+					switch {
+					case result[0].CanUint():
+						for true {
+							v := result[0].Uint()
+							if v == 0 && rejectZero {
+								continue
+							}
+							arg = strconv.FormatUint(v, 10)
+							break
+						}
+
+					case result[0].CanInt():
+						for true {
+							v := result[0].Int()
+							if v == 0 && rejectZero {
+								continue
+							}
+							arg = strconv.FormatInt(v, 10)
+							break
+						}
+					default:
+						arg = result[0].String()
+
+						if _, err := strconv.Atoi(arg); err == nil {
+							// If we get an integer value back, lets just quote it.
+							arg = fmt.Sprintf("\"%s\"", v)
+						}
+					}
 
 					args = append(args, data.Key, arg)
 				} else {
@@ -46,14 +82,6 @@ func GetJsonArrayForResource(r *resources.Resource) []string {
 		}
 
 	}
-
-	for k, v := range args {
-		if _, err := strconv.Atoi(v); err == nil {
-			// If we get an integer value back, lets just quote it.
-			args[k] = fmt.Sprintf("\"%s\"", v)
-		}
-	}
-
 	return args
 
 }
