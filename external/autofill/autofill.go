314c8fba6a4d5726fda8225f7fb90f7c2da99f09
Steve Ramage <steve.ramage@elasticpath.com>
2025-11-03 12:03:18 -0800

    feat: add more template functions for randomization, fake data, fix a panic (Resolves #598)

--- external/autofill/autofill.go
+++ external/autofill/autofill.go
@@ -1,19 +1,11 @@
 package autofill
 
 import (
-	"fmt"
-	"reflect"
-	"strconv"
 	"strings"
 
-	"github.com/brianvoe/gofakeit/v6"
+	"github.com/elasticpath/epcc-cli/external/faker"
 	"github.com/elasticpath/epcc-cli/external/resources"
-	log "github.com/sirupsen/logrus"
 )
-
-var faker = gofakeit.New(0)
-
-var zeroValue = reflect.Value{}
 
 func GetAutoFillQueryParameters(resourceName string, qps []resources.QueryParameter) []string {
 	args := make([]string, 0)
@@ -21,85 +13,35 @@ func GetAutoFillQueryParameters(resourceName string, qps []resources.QueryParame
 	for _, v := range qps {
 		key := v.Name
 		autoFill := v.AutoFill
-		args = processAutoFill(resourceName, autoFill, args, key, key)
+		args = processAutoFill(autoFill, args, key)
 	}
 
 	return args
 }
+
 func GetJsonArrayForResource(r *resources.Resource) []string {
 
 	args := make([]string, 0)
 
-	for attributeName, data := range r.Attributes {
+	for _, data := range r.Attributes {
 		key := data.Key
 		key = strings.Replace(key, "data[n]", "data[0]", 1)
 		autofill := data.AutoFill
 
-		args = processAutoFill(r.SingularName, autofill, args, key, attributeName)
+		args = processAutoFill(autofill, args, key)
 
 	}
 	return args
 
 }
 
-func processAutoFill(resourceName string, autofill string, args []string, key string, attributeName string) []string {
+func processAutoFill(autofill string, args []string, key string) []string {
 	if strings.HasPrefix(autofill, "FUNC:") {
 
-		v := reflect.ValueOf(faker)
 		methodName := strings.Trim(autofill[5:], " ")
 
-		var rejectZero = false
-		if strings.HasPrefix(methodName, "NonZero") {
-			methodName = methodName[7:]
-			rejectZero = true
-		}
-
-		method := v.MethodByName(methodName)
-		if method.IsValid() {
-			result := method.Call([]reflect.Value{})
-			if len(result) == 1 {
-
-				var arg string
-
-				switch {
-				case result[0].CanUint():
-					for true {
-						v := result[0].Uint()
-						if v == 0 && rejectZero {
-							result = method.Call([]reflect.Value{})
-							continue
-						}
-						arg = strconv.FormatUint(v, 10)
-						break
-					}
-
-				case result[0].CanInt():
-					for true {
-						v := result[0].Int()
-						if v == 0 && rejectZero {
-							result = method.Call([]reflect.Value{})
-							continue
-						}
-						arg = strconv.FormatInt(v, 10)
-						break
-					}
-				default:
-					arg = result[0].String()
-
-					if _, err := strconv.Atoi(arg); err == nil {
-						// If we get an integer value back, lets just quote it.
-						arg = fmt.Sprintf("\"%s\"", arg)
-					}
-				}
-
-				args = append(args, key, arg)
-			} else {
-				log.Warnf("Got unexpected number of results from calling %s -> %d", methodName, len(result))
-			}
-
-		} else {
-			log.Warnf("Could not find autofill method %s for attribute %s on resource %s", methodName, attributeName, resourceName)
-		}
+		v := faker.CallFakeFunc(methodName)
+		args = append(args, key, v)
 
 	} else if strings.HasPrefix(autofill, "VALUE:") {
 		args = append(args, key, strings.Trim(autofill[6:], " "))
--- external/templates/funcs.go
+++ external/templates/funcs.go
@@ -1,35 +1,222 @@
 package templates
 
-import "math/rand"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"sort"
+	"sync"
+	"time"
 
-// randString is the internal function that generates a random string.
-// It takes the length of the string and a string of allowed characters as parameters.
+	"github.com/elasticpath/epcc-cli/external/faker"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cast"
+)
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// toFloat64 converts 64-bit floats
+func toFloat64(v any) float64 {
+	return cast.ToFloat64(v)
+}
+
+func toInt(v any) int {
+	return cast.ToInt(v)
+}
+
+// toInt64 converts integer types to 64-bit integers
+func toInt64(v any) int64 {
+	return cast.ToInt64(v)
+}
+
+// RandString is the internal function that generates a random string. It takes the length of the string and a string of allowed characters as parameters.
 func RandString(letters string, n int) string {
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = letters[r.Intn(len(letters))]
 	}
 	return string(b)
 }
 
-// randAlphaNum generates a string consisting of characters in the range 0-9, a-z, and A-Z.
+// RandAlphaNum generates a string consisting of characters in the range 0-9, a-z, and A-Z.
 func RandAlphaNum(n int) string {
 	const letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	return RandString(letters, n)
 }
 
-// randAlpha generates a string consisting of characters in the range a-z and A-Z.
+// RandAlpha generates a string consisting of characters in the range a-z and A-Z.
 func RandAlpha(n int) string {
 	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	return RandString(letters, n)
 }
 
-// randNumeric generates a string consisting of characters in the range 0-9.
+// RandNumeric generates a string consisting of characters in the range 0-9.
 func RandNumeric(n int) string {
 	const digits = "0123456789"
 	return RandString(digits, n)
 }
 
-func RandInt(min, max int) int {
-	return rand.Intn(max-min) + min
+// RandInt returns a value between the min (inclusive) and max (exclusive)
+func RandInt(minA, maxA any) int {
+
+	minN := toInt(minA)
+	maxN := toInt(maxA)
+
+	return r.Intn(maxN-minN) + minN
+}
+
+// RandNorm returns a normal sample with set mean and std deviation.
+func RandNorm(meanA any, stdDevA any) float64 {
+	mean := toFloat64(meanA)
+	stdDev := toFloat64(stdDevA)
+
+	return r.NormFloat64()*stdDev + mean
+}
+
+var mutex sync.Mutex
+
+var sampler = make(map[string][]time.Time)
+
+func Fake(string string) string {
+	return faker.CallFakeFunc(string)
+}
+
+func Seed(x any) string {
+	n := toInt64(x)
+
+	faker.Seed(n)
+	r = rand.New(rand.NewSource(n))
+	return ""
+}
+
+// NRandInt returns n unique values between the min (inclusive) and max (exclusive)
+func NRandInt(nAny, minAny, maxAny any) []int {
+
+	n := toInt(nAny)
+	minInt := toInt(minAny)
+	maxInt := toInt(maxAny)
+
+	if n > (maxInt - minInt) {
+		return []int{}
+	} else if n < 0 {
+		return []int{}
+	}
+
+	// If the range is small enough, lets just generate a permutation
+	if n < 2048 && (maxInt-minInt) < 4096 {
+		nRange := maxInt - minInt
+		perm := r.Perm(nRange)
+
+		v := make([]int, n)
+
+		for i := 0; i < min(n, nRange); i++ {
+			v[i] = perm[i] + minInt
+		}
+		return v
+	} else {
+		// Otherwise, lets just generate a set of random numbers
+		// This algorithm uses rejection sampling and is maybe terrible when n~=(max-min)
+		v := map[int]struct{}{}
+
+		for len(v) < n {
+			v[RandInt(minInt, maxInt)] = struct{}{}
+		}
+
+		results := make([]int, 0, n)
+		for k := range v {
+			results = append(results, k)
+		}
+
+		sort.Ints(results)
+		return results
+	}
+}
+
+func FormatPrice(currency string, pAny any) string {
+
+	p := toInt64(pAny)
+
+	symbol := "£"
+	if currency == "USD" {
+		symbol = "$"
+	}
+
+	return fmt.Sprintf("%s%d.%02d", symbol, p/100, p%100)
+}
+
+func WeightedDateTimeSampler(start string, end string) string {
+
+	key := fmt.Sprintf("%s_%s", start, end)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if computedTable, ok := sampler[key]; ok {
+		t := computedTable[r.Intn(len(computedTable))]
+
+		t = t.Add(time.Duration(RandInt(0, 3600)) * time.Second)
+		return t.Format(time.RFC3339)
+	}
+
+	startTime, err := parseAny(start)
+	if err != nil {
+		log.Fatalf("failed to parse start time: %v", err)
+	}
+
+	endTime, err := parseAny(end)
+	if err != nil {
+		log.Fatalf("failed to parse start time: %v", err)
+	}
+
+	if !startTime.Before(endTime) {
+		log.Fatalf("start %s must be before end, %s", startTime, endTime)
+	}
+
+	hours := endTime.Sub(startTime).Hours()
+
+	weightedHours := make([]int, int(hours))
+
+	// Actually compute the day of the week
+	startDayOfWeek := 0.0
+
+	for idx := range weightedHours {
+		// Each hour has uniform weight
+
+		hour := float64(idx % 24)
+		day := float64((idx / 24) % 7)
+		hourlyBias := 3 * math.Cos(2*math.Pi*((hour+4)/24))               // daily cycle
+		dayOfWeekBias := 2 * math.Cos(2*math.Pi*((day+startDayOfWeek)/7)) // weekly cycle
+
+		//hourlyBias := ((idx % 24) / 2)
+		//dayOfWeekBias := (idx/24)%7 + startDayOfWeek
+		weekBias := idx * 4 / int(hours)
+
+		weightedHours[idx] = int(math.Max(hourlyBias+dayOfWeekBias+float64(weekBias), 1))
+	}
+
+	sum := 0
+
+	lookup := make([]time.Time, 0)
+
+	currentBlock := startTime
+	for _, v := range weightedHours {
+		sum += v
+		for j := 0; j < v; j++ {
+			lookup = append(lookup, currentBlock)
+		}
+
+		currentBlock = currentBlock.Add(time.Duration(1) * time.Hour)
+	}
+
+	sampler[key] = lookup
+
+	t := lookup[r.Intn(len(lookup))]
+	t = t.Add(time.Duration(RandInt(0, 3600)) * time.Second)
+	return t.Format(time.RFC3339)
+}
+
+func parseAny(s string) (time.Time, error) {
+	if t, err := time.Parse("2006-01-02", s); err == nil {
+		return t, nil
+	}
+	return time.Time{}, fmt.Errorf("unsupported time format: %q", s)
 }
