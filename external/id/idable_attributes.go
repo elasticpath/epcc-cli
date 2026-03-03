c46cd43c419eb5cac22d669a4c000b6d619c97ec
Steve Ramage <49958178+steve-r-west@users.noreply.github.com>
2026-03-03 11:56:47 -0800

    fix: change delete-all default page length and get-all json output (Resolves #661) (#662)

--- external/id/idable_attributes.go
+++ external/id/idable_attributes.go
@@ -1,9 +1,9 @@
 package id
 
 type IdableAttributes struct {
-	Id          string `yaml:"id"`
-	Slug        string `yaml:"slug,omitempty"`
-	Sku         string `yaml:"sku,omitempty"`
-	Code        string `yaml:"code,omitempty"`
-	ExternalRef string `yaml:"external_ref,omitempty"`
+	Id          string `yaml:"id" json:"id"`
+	Slug        string `yaml:"slug,omitempty" json:"slug,omitempty"`
+	Sku         string `yaml:"sku,omitempty" json:"sku,omitempty"`
+	Code        string `yaml:"code,omitempty" json:"code,omitempty"`
+	ExternalRef string `yaml:"external_ref,omitempty" json:"external_ref,omitempty"`
 }
